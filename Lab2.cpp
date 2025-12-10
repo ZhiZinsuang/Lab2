fb331fce08d355ca52c5c81ce58b9164da27b8bf
Ulyana <aloe1502@gmail.com>
2025-12-10 11:10:56 +0700

    Созданы классы Names и Table_names

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,20 +1,113 @@
-﻿// Lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class names {
+public:
+    string name;
+    int win;
+    int lose;
+    int allplays;
+public:
+    names() : name("player"), win(0), lose(0), allplays(0) {}
+    names(string name, int win, int lose, int allp) : name(name), win(win), lose(lose), allplays(allp) {}
+    ~names() {}
+};
+
+class table_names {
+public:
+    string file_r;
+public:
+    table_names() {}
+    table_names(string f) : file_r(f) {}
+    void outTable();
+    vector<names>& readfile();
+};
+
+void table_names::outTable() {
+    vector<names> list;
+
+    ifstream file(file_r);
+    if (!file.is_open())
+        cout << "Не удалось открыть файл." << endl;
+    else {
+        string line;
+        string line1, line2, line3, line4;
+        while (std::getline(file, line)) {
+            int i = 0;
+            char delimiter = ' ';
+            size_t start = 0;
+            size_t end = line.find(delimiter);
+            while (end != string::npos) {
+                if (i == 0)
+                    line1 = line.substr(start, end - start);
+                else if (i == 1)
+                    line2 = line.substr(start, end - start);
+                else if (i == 2)
+                    line3 = line.substr(start, end - start);
+                start = end + 1;
+                end = line.find(delimiter, start);
+                i++;
+            }
+            line4 = line.substr(start);
+
+            list.push_back(names(line1, stoi(line2), stoi(line3), stoi(line4)));
+        }
+    }
+    file.close();
+
+    // печать
+    for (const auto& p : list) {
+        cout << p.name << endl;
+    }
+}
+
+vector<names>& table_names::readfile() {
+    vector<names> list;
+
+    ifstream file(file_r);
+    if (!file.is_open()) 
+        cout << "Не удалось открыть файл." << endl;
+    else {
+        string line;
+        while (std::getline(file, line)) {
+            //cout << line << endl;
+
+            string line1, line2, line3, line4;
+            int i = 0;
+            char delimiter = ' ';
+            size_t start = 0;
+            size_t end = line.find(delimiter);
+            while (end != string::npos) {
+                if (i == 0)
+                    line1 = line.substr(start, end - start);
+                else if (i == 1)
+                    line2 = line.substr(start, end - start);
+                else if (i == 2)
+                    line3 = line.substr(start, end - start);
+                //std::cout << line.substr(start, end - start) << std::endl;
+                start = end + 1;
+                end = line.find(delimiter, start);
+                i++;
+            }
+            line4 = line.substr(start);
+            //std::cout << line.substr(start) << std::endl; // Последняя часть
+            
+            list.push_back(names(line1, stoi(line2), stoi(line3), stoi(line4)));
+            
+        }
+    }
+    file.close();
+    return list;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    table_names n("tableNames.txt");
+    n.outTable();
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
