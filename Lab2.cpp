9008709612ac310f041ebeea46f69d36cd6c9d90
Ulyana <aloe1502@gmail.com>
2025-12-12 21:03:19 +0700

    Добавлен id, файл читается при создании обекта и переписывается при удалении

--- Lab2.cpp
+++ Lab2.cpp
@@ -9,15 +9,21 @@ using namespace std;
 
 class user {
 private:
+    int id;
     string name;
     int win;
     int lose;
     int allplays;
 public:
-    user() : name("player"), win(0), lose(0), allplays(0) {}
-    user(string name) : name(name), win(0), lose(0), allplays(0) {}
-    user(string name, int win, int lose, int allp) : name(name), win(win), lose(lose), allplays(allp) {}
+    user() : id(0), name("player"), win(0), lose(0), allplays(0) {}
+    //user(string name) : name(name), win(0), lose(0), allplays(0) { }
+    user(string name, int id_b) : id(0), name(name), win(0), lose(0), allplays(0) {
+        if (id_b != 0)
+            id = id_b + 1;
+    }
+    user(int id, string name, int win, int lose, int allp) : id(id), name(name), win(win), lose(lose), allplays(allp) {}
     ~user() {}
+    int getId() { return id; }
     string getName() { return name; }
     int getWin() { return win; }
     int getLose() { return lose; }
@@ -30,22 +36,27 @@ private:
     vector<user> users;
 public:
     table_users() {}
-    table_users(string f) : file_r(f) {}
-    ~table_users() {}
-    void out();     //выводит данные 
+    table_users(string f) : file_r(f) { 
+        readfile(); 
+    }
+    ~table_users() { 
+        rewrightFile(); 
+    }
+    void out();     //выводит данные из массива в консоль
     void readfile();    //считывает файл в массив vector<users>
-    void newUser(string name);      //добавляет в файл нового пользователя
+    void newUser(string name);      //добавляет в файл/массив нового пользователя
     bool duplicateName(string name);     //проверяет, создан ли уже такой пользователь
-
+    int countUsers();       //считает кол-во пользователей в массиве
+    void rewrightFile();
 };
 
-//выводит данные
+//выводит данные из массива в консоль
 void table_users::out(){
-    readfile();
+    //readfile();
     for (user p : users) {
-        cout << p.getName() << " " << p.getWin() << " " << p.getLose() << " " << p.getAllplays() << endl;
+        cout << p.getId() << " " <<  p.getName() << " " << p.getWin() << " " << p.getLose() << " " << p.getAllplays() << endl;
     }
-    users.clear();
+    //users.clear();
 }
 
 //считывает файл в массив vector<users>
@@ -55,7 +66,7 @@ void table_users::readfile() {
         cout << "Не удалось открыть файл." << endl;
     else {
         string line;
-        string line1, line2, line3, line4;
+        string line1, line2, line3, line4, line5;
         while (getline(file, line)) {
             int i = 0;
             char delimiter = ' ';
@@ -68,20 +79,21 @@ void table_users::readfile() {
                     line2 = line.substr(start, end - start);
                 else if (i == 2)
                     line3 = line.substr(start, end - start);
+                else if (i == 3)
+                    line4 = line.substr(start, end - start);
                 start = end + 1;
                 end = line.find(delimiter, start);
                 i++;
             }
-            line4 = line.substr(start);
+            line5 = line.substr(start);
             
-            users.push_back(user(line1, stoi(line2), stoi(line3), stoi(line4)));
+            users.push_back(user(stoi(line1), line2, stoi(line3), stoi(line4), stoi(line5)));
         }
     }
     file.close();
 }
 
 //проверяет, есть ли в файле такое имя
-
 bool table_users::duplicateName(string name) {
     if (users.empty())
         readfile();
@@ -91,12 +103,12 @@ bool table_users::duplicateName(string name) {
             return true;
         i++;
     }
-    users.clear();  //очищаем оперативную память от массива воизбежание ошибок
+    //users.clear();  //очищаем оперативную память от массива воизбежание ошибок
     return false;
 }
 
 //добавляет в файл нового пользователя
-void table_users::newUser(string name) {
+/*void table_users::newUser(string name) {
     //проверка на повторность имени
     if (duplicateName(name))
         cout << "Такой пользователь уже есть" << endl;
@@ -113,6 +125,34 @@ void table_users::newUser(string name) {
         }
         out.close();
     }
+}*/
+
+//добавляет нового пользователя в массив
+void table_users::newUser(string name) {
+    if (duplicateName(name))    //проверка на повторность имени
+        cout << "Такой пользователь уже есть" << endl;
+    else {
+        users.push_back(user(name, countUsers()));
+    }
+}
+
+int table_users::countUsers() {
+    return users.size();
+}
+
+void table_users::rewrightFile() {
+    ofstream out;
+    out.open(file_r);
+    if (out.is_open())
+    {
+        for (user u : users) {
+            out << u.getId() << " " << u.getName() << " " << u.getWin() << " " << u.getLose() << " " << u.getAllplays() << endl;
+        }
+    }
+    else {
+        cout << "ошибка открытия файла" << endl;
+    }
+    out.close();
 }
 
 int main()
@@ -122,8 +162,9 @@ int main()
     SetConsoleOutputCP(1251);
 
     table_users n("tableUsers.txt");
-    
-    n.newUser("hoo");
+
+    n.newUser("hello");
     n.out();
+    n.rewrightFile();
 }
 
