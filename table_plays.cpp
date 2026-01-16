25542c1f4c4d2423a71037df06d293d639d9179f
Ulyana <aloe1502@gmail.com>
2026-01-16 21:43:17 +0700

    Дополнение к предыдущему коммиту

--- table_plays.cpp
+++ table_plays.cpp
@@ -1,120 +1,148 @@
+// table_plays.cpp
 #include <iostream>
-#include <memory>
 #include <vector>
 #include <fstream>
-#include <string>
-
+#include <sstream>
+#include <memory>
+#include <algorithm>
 #include "table_plays.h"
 #include "falseFormatLineException.h"
-#include "nonExistentUserException.h"
 
 using namespace std;
 
 Table_plays::Table_plays() {}
 
-Table_plays::~Table_plays() {
-    rewrightFile();
-}
-
-Table_plays::Table_plays(string f) : file_r(f) {
-    try { 
-        readfile(); 
+Table_plays::Table_plays(const string& f) : file_r(f) {
+    try {
+        readfile();
     }
-    catch (FalseFormatLineException& e) {
+    catch (const FalseFormatLineException& e) {
         cout << e.getMessage() << endl;
     }
 }
 
+Table_plays::Table_plays(const Table_plays& other) : file_r(other.file_r) {
+    // Глубокое копирование shared_ptr
+    for (const auto& play : other.plays) {
+        plays.push_back(make_shared<Play>(*play));
+    }
+}
+
+Table_plays::~Table_plays() {
+    rewrightFile();
+}
+
 void Table_plays::readfile() {
     ifstream file(file_r);
-    if (!file.is_open())
+    if (!file.is_open()) {
         cout << "Не удалось открыть файл." << endl;
-    else {
-        try {
-            string line;
-            string line1, line2, line3;
-            while (getline(file, line)) {
-                int i = 0;
-                char delimiter = ' ';
-                size_t start = 0;
-                size_t end = line.find(delimiter);
-                while (end != string::npos) {
-                    if (i == 0)
-                        line1 = line.substr(start, end - start);
-                    else if (i == 1)
-                        line2 = line.substr(start, end - start);
-                    start = end + 1;
-                    end = line.find(delimiter, start);
-                    i++;
-                }
-                line3 = line.substr(start);
+        return;
+    }
 
-                plays.push_back(Play(stoi(line1), line2, line3));
+    try {
+        string line;
+        while (getline(file, line)) {
+            // Используем stringstream для более безопасного парсинга
+            stringstream ss(line);
+            string line1, line2, line3;
+
+            if (!(ss >> line1 >> line2 >> line3)) {
+                throw FalseFormatLineException("Строка в файле неверного формата: " + line);
             }
-        }catch (...) {
-            throw FalseFormatLineException("Строка в файле неверного формата");
+
+            // Проверяем, есть ли еще данные в строке
+            string extra;
+            if (ss >> extra) {
+                throw FalseFormatLineException("Лишние данные в строке: " + line);
+            }
+
+            plays.push_back(make_shared<Play>(stoi(line1), line2, line3));
         }
     }
+    catch (const exception& e) {
+        throw FalseFormatLineException(string("Ошибка чтения файла: ") + e.what());
+    }
+
     file.close();
 }
 
-void Table_plays::outPlays() {
-    if (plays.size() == 0)
+void Table_plays::outPlays() const {
+    if (plays.empty()) {
         cout << "Данных нет" << endl;
+    }
     else {
-        for (Play p : plays) {
-            cout << p.getId() << " " << p.getUserWin() << " " << p.getUserLose() << endl;
+        for (const auto& p : plays) {
+            cout << *p << endl;
         }
     }
 }
 
 void Table_plays::rewrightFile() {
-    ofstream out;
-    out.open(file_r);
-    if (out.is_open())
-    {
-        for (Play p : plays) {
-            out << p.getId() << " " << p.getUserWin() << " " << p.getUserLose() << endl;
-        }
+    ofstream out(file_r);
+    if (!out.is_open()) {
+        cout << "Ошибка открытия файла" << endl;
+        return;
     }
-    else {
-        cout << "ошибка открытия файла" << endl;
+
+    for (const auto& p : plays) {
+        out << p->getId() << " " << p->getUserWin() << " " << p->getUserLose() << endl;
     }
+
     out.close();
 }
 
-int Table_plays::countPlays() {
+int Table_plays::countPlays() const {
     return static_cast<int>(plays.size());
 }
 
- shared_ptr<User> Table_plays::getUserByName(Table_users& usersTable, string& name) const {
-     for (int i = 0; i < usersTable.countUsers(); ++i) {
-         auto user = usersTable.getUser(i);
-         if (user->getName() == name) {
-             return user;  // Возвращаем найденного пользователя
-         }
-     }
-     throw NonExistentUserException("Пользователь '" + name + "' не найден");
- }
+void Table_plays::newPlay(const string& uWin, const string& uLose) {
+    int id = countPlays();
+    plays.push_back(make_shared<Play>(id, uWin, uLose));
+}
 
- void Table_plays::newPlay(string uWin, string uLose, Table_users& usersTable) {
-     int id = countPlays();
-     plays.emplace_back(id, uWin, uLose);  // Добавляем игру
+Table_plays& Table_plays::operator=(const Table_plays& other) {
+    if (this != &other) {
+        file_r = other.file_r;
+        plays.clear();
+        for (const auto& play : other.plays) {
+            plays.push_back(make_shared<Play>(*play));
+        }
+    }
+    return *this;
+}
 
-     try {
-         // Находим победителя и обновляем статистику
-         //const auto& user1 = getUserByName(usersTable, uWin);  // Ссылка на возвращаемый shared_ptr
-         //user1->userWin();
-         auto user1 = getUserByName(usersTable, uWin);  // Ссылка на возвращаемый shared_ptr
-         user1->userWin();
+shared_ptr<Play> Table_plays::operator[](size_t index) {
+    if (index >= plays.size()) {
+        throw out_of_range("Index out of range");
+    }
+    return plays[index];
+}
 
-         // Находим проигравшего и обновляем статистику  
-         //const auto& user2 = getUserByName(usersTable, uLose);
-         //user2->userLose(); // userLose() вызывается на КОПИИ!
-     }
-     catch (NonExistentUserException& e) {
-         cout << "Ошибка: " << e.getMessage() << endl;
-         // Откатываем добавление игры при ошибке
-         plays.pop_back();
-     }
- }
+const shared_ptr<Play> Table_plays::operator[](size_t index) const {
+    if (index >= plays.size()) {
+        throw out_of_range("Index out of range");
+    }
+    return plays[index];
+}
+
+ostream& operator<<(ostream& os, const Table_plays& table) {
+    os << "Table Plays (" << table.countPlays() << " records):" << endl;
+    for (size_t i = 0; i < table.plays.size(); ++i) {
+        os << "  " << i + 1 << ". " << *table.plays[i] << endl;
+    }
+    return os;
+}
+
+vector<shared_ptr<Play>> Table_plays::findPlaysByPlayer(const string& playerName) const {
+    vector<shared_ptr<Play>> result;
+
+    // Поиск с использованием алгоритмов STL и лямбда-функций
+    copy_if(plays.begin(), plays.end(), back_inserter(result),
+        [&playerName](const shared_ptr<Play>& play) {
+            // Используем find для поиска подстроки в именах игроков
+            return play->getUserWin().find(playerName) != string::npos ||
+                play->getUserLose().find(playerName) != string::npos;
+        });
+
+    return result;
+}
--- table_plays.h
+++ table_plays.h
@@ -1,29 +1,37 @@
+// table_plays.h
 #ifndef TABLE_PLAYS_H
 #define TABLE_PLAYS_H
 
 #include <iostream>
 #include <vector>
 #include <memory>
-
+#include <string>
 #include "play.h"
-#include "table_users.h"
-
-using namespace std;
 
 class Table_plays {
 private:
-    string file_r;
-    vector<Play> plays;
-    shared_ptr<User> getUserByName(Table_users& usersTable, string& name) const;
+    std::string file_r;
+    std::vector<std::shared_ptr<Play>> plays; // Используем shared_ptr
 public:
     Table_plays();
-    Table_plays(string f);
+    Table_plays(const std::string& f);
+    Table_plays(const Table_plays& other); // Конструктор копирования
     ~Table_plays();
-    void outPlays();
+
+    void outPlays() const;
     void readfile();
     void rewrightFile();
-    int countPlays();
-    void newPlay(string uWin, string uLose, Table_users& usersTable);
+    int countPlays() const;
+    void newPlay(const std::string& uWin, const std::string& uLose);
+
+    // Перегрузка операторов
+    Table_plays& operator=(const Table_plays& other);
+    std::shared_ptr<Play> operator[](size_t index);
+    const std::shared_ptr<Play> operator[](size_t index) const;
+    friend std::ostream& operator<<(std::ostream& os, const Table_plays& table);
+
+    // Поиск по имени игрока
+    std::vector<std::shared_ptr<Play>> findPlaysByPlayer(const std::string& playerName) const;
 };
 
 #endif
