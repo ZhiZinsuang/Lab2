5db0226b9447c83c10d4e5707f2be6167ccade76
Ulyana <aloe1502@gmail.com>
2026-01-16 21:42:22 +0700

    Изменены под требования lab5 классы Play и Table_plays

--- play.cpp
+++ play.cpp
@@ -1,20 +1,58 @@
+// play.cpp
 #include <iostream>
+#include <sstream>
 #include "play.h"
 
 using namespace std;
 
-Play::Play(int id, string userWin, string userLose) :id(id), userWin(userWin), userLose(userLose) {}
+Play::Play(int id, const string& userWin, const string& userLose)
+    : id(id), userWin(userWin), userLose(userLose) {}
+
+Play::Play(const Play& other)
+    : id(other.id), userWin(other.userWin), userLose(other.userLose) {}
 
 Play::~Play() {}
 
-int Play::getId() {
+int Play::getId() const {
     return id;
 }
 
-string Play::getUserWin() {
+string Play::getUserWin() const {
     return userWin;
 }
 
-string Play::getUserLose() {
+string Play::getUserLose() const {
     return userLose;
+}
+
+// Перегрузка оператора присваивания
+Play& Play::operator=(const Play& other) {
+    if (this != &other) {
+        id = other.id;
+        userWin = other.userWin;
+        userLose = other.userLose;
+    }
+    return *this;
+}
+
+bool Play::operator==(const Play& other) const {
+    return id == other.id &&
+        userWin == other.userWin &&
+        userLose == other.userLose;
+}
+
+bool Play::operator!=(const Play& other) const {
+    return !(*this == other);
+}
+
+ostream& operator<<(ostream& os, const Play& play) {
+    os << "Play ID: " << play.id
+        << ", Winner: " << play.userWin
+        << ", Loser: " << play.userLose;
+    return os;
+}
+
+string combinePlayInfo(const Play& play) {
+    return "Match #" + to_string(play.id) + ": " +
+        play.userWin + " vs " + play.userLose;
 }
--- play.h
+++ play.h
@@ -1,21 +1,33 @@
+// play.h
 #ifndef PLAY_H
 #define PLAY_H
 
 #include <iostream>
-
-using namespace std;
+#include <string>
+#include <memory>
 
 class Play {
 private:
     int id;
-    string userWin;
-    string userLose;
+    std::string userWin;
+    std::string userLose;
 public:
-    Play(int id, string userWin, string userLose);
+    Play(int id, const std::string& userWin, const std::string& userLose);
+    Play(const Play& other); // Конструктор копирования
     ~Play();
-    int getId();
-    string getUserWin();
-    string getUserLose();
+
+    int getId() const;
+    std::string getUserWin() const;
+    std::string getUserLose() const;
+
+    // Перегрузка операторов
+    Play& operator=(const Play& other);
+    bool operator==(const Play& other) const;
+    bool operator!=(const Play& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Play& play);
+
+    // Дружественная функция для конкатенации информации
+    friend std::string combinePlayInfo(const Play& play);
 };
 
 #endif
